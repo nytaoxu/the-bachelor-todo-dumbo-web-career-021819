f67667ec05c83bb1ff0d48d9711faa1d71a872aa
Tao Xu <nytaoxu@gmail.com>
2019-02-14 19:51:14 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,6 +1,8 @@
 def get_first_name_of_season_winner(data, season)
   # code here
-  data.each do |season, 
+  data.each do |season, players|
+    
+  end
 end
 
 def get_contestant_name(data, occupation)
